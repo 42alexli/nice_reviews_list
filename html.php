12b4cae9c6a15330d3094633a1cb99cf4a184a7c
42alexli <alexshulga@MacBook-Pro-Alex.local>
2022-10-16 14:51:19 +0200

    Adding data to html via get method

--- html.php
+++ html.php
@@ -24,47 +24,76 @@ if ( class_exists( 'AlexShulhaDeveloper' ) ) {
                     Play
                 </div>
             </div>
-            <div class="d-flex align-items-center row g-4 row-cols-1 row-cols-lg-3 items-data">
+			<?php
+			/** Assign the data array from the class to the $data variable using the getdata() method */
+			$data = $alexshdev->get_data();
+			if(is_array($data)):
+				foreach ( $data as $item ):
+					?>
+                    <div class="d-flex align-items-center row g-4 row-cols-1 row-cols-lg-3 items-data">
 
-                <div class="item p-2 col-md-6 col-lg-3">
-                    <a href="/49211">
-                        <div class="item__first text-center py-4">
-                            <img src="https://picsum.photos/195/75" alt="party-casino-paypal">
+                        <div class="item p-2 col-md-6 col-lg-3">
+                            <a href="/<?php echo $item->brand_id ? $item->brand_id : ''; ?>">
+                                <div class="item__first text-center py-4">
+									<?php if(!empty($item->logo)): ?>
+                                        <img src="<?php echo $item->logo; ?>"
+                                             alt="party-casino-paypal">
+									<?php endif;?>
+                                </div>
+                                <div class="item__second text-center">
+                                    Review
+                                </div>
+                            </a>
                         </div>
-                        <div class="item__second text-center">
-                            Review
+
+                        <div class="item p-2 col-md-6 col-lg-3">
+                            <div class="item__first">
+								<?php if(!empty($item->info->rating)): ?>
+                                    <div class="rating-result text-center py-3">
+										<?php
+										/** Creating a range from 1 to 5 and iterate through the range
+										 * comparing with the rating obtained from JSON to make a rating with stars
+										 */
+										foreach ( range( 1, 5 ) as $number ) {
+											$class = $item->info->rating >= $number ? 'active' : '';
+											echo '<span class="' . $class . '"></span>';
+										} ?>
+                                    </div>
+								<?php endif;?>
+                            </div>
+							<?php if(!empty($item->info->bonus) ): ?>
+                                <div class="item__second text-center px-5">
+                                    <p><?php echo $item->info->bonus; ?></p>
+                                </div>
+							<?php endif;?>
                         </div>
-                    </a>
-                </div>
+						<?php if(is_array($item->info->features) ): ?>
+                            <div class="item p-2 col-md-6 col-lg-3">
+                                <div class="item__first text-center">
+                                    <ul>
+										<?php foreach ( $item->info->features as $feature ) { ?>
+                                            <li><?php echo $feature; ?></li>
+										<?php } ?>
+                                    </ul>
+                                </div>
+                            </div>
+						<?php endif;?>
 
-                <div class="item p-2 col-md-6 col-lg-3">
-                    <div class="item__first">
-                        <div class="rating-result text-center py-3">
-                            <span class="active"></span><span class="active"></span><span class="active"></span><span class=""></span><span class=""></span>                            </div>
+                        <div class="item p-2 col-md-6 col-lg-3">
+							<?php if(!empty($item->play_url) ): ?>
+                                <div class="item__first text-center pb-3">
+                                    <a class="btn btn-lg btn-block btn-primary px-5" href="<?php echo $item->play_url; ?>">Play now</a>
+                                </div>
+							<?php endif;?>
+							<?php if(!empty($item->terms_and_conditions) ): ?>
+                                <div class="item__second text-center">
+                                    <p class="terms_and_conditions"><?php echo $item->terms_and_conditions; ?></p>
+                                </div>
+							<?php endif;?>
+                        </div>
                     </div>
-                    <div class="item__second text-center px-5">
-                        <p>Free $10 + 100% deposit bonus up to $600</p>
-                    </div>
-                </div>
-                <div class="item p-2 col-md-6 col-lg-3">
-                    <div class="item__first text-center">
-                        <ul>
-                            <li>Several limited promos</li>
-                            <li>$100 no deposit bonus</li>
-                            <li>App for android &amp; iOS</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="item p-2 col-md-6 col-lg-3">
-                    <div class="item__first text-center pb-3">
-                        <a class="btn btn-lg btn-block btn-primary px-5" href="https://example.com/wow-the-perfect-casino">Play now</a>
-                    </div>
-                    <div class="item__second text-center">
-                        <p class="terms_and_conditions">18+ | <a href="https://generator.lorem-ipsum.info/terms-and-conditions">T&amp;CS Apply</a> | Gamble Responsibly</p>
-                    </div>
-                </div>
-            </div>
+				<?php endforeach; ?>
+			<?php endif;?>
 
         </div>
     </div>
